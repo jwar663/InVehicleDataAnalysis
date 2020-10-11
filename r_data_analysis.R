d884b49cd276d4bbae9362e55bfc5a48f74e60c8
jwar663 <32050015+jwar663@users.noreply.github.com>
2020-10-11 18:53:58 +1300

    Added two way ANOVA, now including dashboard or gesture first

--- r_data_analysis.R
+++ r_data_analysis.R
@@ -46,9 +46,18 @@ hist(driving.data$DRIVING_ERRORS)
 hist(driving.data$INTERACTION_ERRORS)
 
 #one way ANOVA for each variable
+#the larger the f-value the more likely the difference is not due to chance
 one.way.reaction.time <- aov(REACTION_TIME ~ DASHBOARD_GESTURE, data=driving.data)
 one.way.driving.errors <- aov(DRIVING_ERRORS ~ DASHBOARD_GESTURE, data=driving.data)
 one.way.interaction.errors <- aov(INTERACTION_ERRORS ~ DASHBOARD_GESTURE, data=driving.data)
 summary(one.way.reaction.time)
 summary(one.way.driving.errors)
 summary(one.way.interaction.errors)
+
+#two way ANOVA for each variable not considering the impact of completing dashboard first or gesture first
+two.way.reaction.time <- aov(REACTION_TIME ~ DASHBOARD_GESTURE + DASHBOARD_FIRST, data=driving.data)
+two.way.driving.errors <- aov(DRIVING_ERRORS ~ DASHBOARD_GESTURE + DASHBOARD_FIRST, data=driving.data)
+two.way.interaction.errors <- aov(INTERACTION_ERRORS ~ DASHBOARD_GESTURE + DASHBOARD_FIRST, data=driving.data)
+summary(two.way.reaction.time)
+summary(two.way.driving.errors)
+summary(two.way.interaction.errors)
