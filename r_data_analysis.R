df800980c179bbec453d19ca0d2273bdd4922d51
jwar663 <32050015+jwar663@users.noreply.github.com>
2020-10-11 19:10:06 +1300

    finding best fit ANOVA model, and formatting

--- r_data_analysis.R
+++ r_data_analysis.R
@@ -18,55 +18,100 @@ summary(driving.data)
 
 #t-test because only two groups, paired t-test because results are from a single population, one-tailed t-test because direction of difference matters
 #p-value should be < 0.05 to reject null hypothesis
+
 #t-test reaction time
 t.test(REACTION_TIME ~ DASHBOARD_GESTURE, data=driving.data, paired=TRUE)
 #t.test(REACTION_TIME ~ DASHBOARD_GESTURE, data=driving.data)
 
-#t-test because only two groups, paired t-test because results are from a single population, one-tailed t-test because direction of difference matters
-#p-value should be < 0.05 to reject null hypothesis
-#t-test reaction time
+#t-test driving errors
 t.test(DRIVING_ERRORS ~ DASHBOARD_GESTURE, data=driving.data, paired=TRUE)
 #t.test(DRIVING_ERRORS ~ DASHBOARD_GESTURE, data=driving.data)
 
-#t-test because only two groups, paired t-test because results are from a single population, one-tailed t-test because direction of difference matters
-#p-value should be < 0.05 to reject null hypothesis
-#t-test reaction time
+#t-test interaction errors
 t.test(INTERACTION_ERRORS ~ DASHBOARD_GESTURE, data=driving.data, paired=TRUE)
 #t.test(INTERACTION_ERRORS ~ DASHBOARD_GESTURE, data=driving.data)
 
 #display box plots for each different variable
+
+#box plot reaction time
 boxplot(REACTION_TIME ~ DASHBOARD_GESTURE, data=driving.data)
+
+#box plot driving errors
 boxplot(DRIVING_ERRORS ~ DASHBOARD_GESTURE, data=driving.data)
+
+#box plot interaction errors
 boxplot(INTERACTION_ERRORS ~ DASHBOARD_GESTURE, data=driving.data)
 
 #display histograms of each different variable to check if data is normally distributed
 #if not normally distributed, some processing may be required for data
+
+#histogram for reaction time
 hist(driving.data$REACTION_TIME)
+
+#histogram for driving errors
 hist(driving.data$DRIVING_ERRORS)
+
+#histogram for interaction errors
 hist(driving.data$INTERACTION_ERRORS)
 
 #one way ANOVA for each variable
 #the larger the f-value the more likely the difference is not due to chance
+
+#one way ANOVA for reaction time
 one.way.reaction.time <- aov(REACTION_TIME ~ DASHBOARD_GESTURE, data=driving.data)
-one.way.driving.errors <- aov(DRIVING_ERRORS ~ DASHBOARD_GESTURE, data=driving.data)
-one.way.interaction.errors <- aov(INTERACTION_ERRORS ~ DASHBOARD_GESTURE, data=driving.data)
 summary(one.way.reaction.time)
+
+#one way ANOVA for driving errors
+one.way.driving.errors <- aov(DRIVING_ERRORS ~ DASHBOARD_GESTURE, data=driving.data)
 summary(one.way.driving.errors)
+
+#one way ANOVA for interaction errors
+one.way.interaction.errors <- aov(INTERACTION_ERRORS ~ DASHBOARD_GESTURE, data=driving.data)
 summary(one.way.interaction.errors)
 
 #two way ANOVA for each variable now considering the impact of completing dashboard first or gesture first
+
+#two way ANOVA for reaction time
 two.way.reaction.time <- aov(REACTION_TIME ~ DASHBOARD_GESTURE + DASHBOARD_FIRST, data=driving.data)
-two.way.driving.errors <- aov(DRIVING_ERRORS ~ DASHBOARD_GESTURE + DASHBOARD_FIRST, data=driving.data)
-two.way.interaction.errors <- aov(INTERACTION_ERRORS ~ DASHBOARD_GESTURE + DASHBOARD_FIRST, data=driving.data)
 summary(two.way.reaction.time)
+
+#two way ANOVA for driving errors
+two.way.driving.errors <- aov(DRIVING_ERRORS ~ DASHBOARD_GESTURE + DASHBOARD_FIRST, data=driving.data)
 summary(two.way.driving.errors)
+
+#two way ANOVA for interaction errors
+two.way.interaction.errors <- aov(INTERACTION_ERRORS ~ DASHBOARD_GESTURE + DASHBOARD_FIRST, data=driving.data)
 summary(two.way.interaction.errors)
 
 #two way ANOVA for each variable now considering the interaction between the two independent variables
 #low sum-of-squares and high p-value means not much variation because of interaction
+
+#two way interaction ANOVA for reaction time
 interaction.reaction.time <- aov(REACTION_TIME ~ DASHBOARD_GESTURE * DASHBOARD_FIRST, data=driving.data)
-interaction.driving.errors <- aov(DRIVING_ERRORS ~ DASHBOARD_GESTURE * DASHBOARD_FIRST, data=driving.data)
-interaction.interaction.errors <- aov(INTERACTION_ERRORS ~ DASHBOARD_GESTURE * DASHBOARD_FIRST, data=driving.data)
 summary(interaction.reaction.time)
+
+#two way interaction ANOVA for driving errors
+interaction.driving.errors <- aov(DRIVING_ERRORS ~ DASHBOARD_GESTURE * DASHBOARD_FIRST, data=driving.data)
 summary(interaction.driving.errors)
+
+#two way interaction ANOVA for interaction errors
+interaction.interaction.errors <- aov(INTERACTION_ERRORS ~ DASHBOARD_GESTURE * DASHBOARD_FIRST, data=driving.data)
 summary(interaction.interaction.errors)
+
+#find the best fitting model from the three that have been created for each variable
+#the lowest AIC values indicates the best fit model
+
+#best fit model for reaction time
+model.set.reaction.time <- list(one.way.reaction.time, two.way.reaction.time, interaction.reaction.time)
+model.names.reaction.time <- c("one.way.reaction.time", "two.way.reaction.time", "interaction.reaction.time")
+aictab(model.set.reaction.time, modnames = model.names.reaction.time)
+
+#best fit model for driving errors
+model.set.driving.errors <- list(one.way.driving.errors, two.way.driving.errors, interaction.driving.errors)
+model.names.driving.errors <- c("one.way.driving.errors", "two.way.driving.errors", "interaction.driving.errors")
+aictab(model.set.driving.errors, modnames = model.names.driving.errors)
+
+#best fit model for interaction errors
+model.set.interaction.errors <- list(one.way.interaction.errors, two.way.interaction.errors, interaction.interaction.errors)
+model.names.interaction.errors <- c("one.way.interaction.errors", "two.way.interaction.errors", "interaction.interaction.errors")
+aictab(model.set.interaction.errors, modnames = model.names.interaction.errors)
